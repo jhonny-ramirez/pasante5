c6800e022e3bbf33b4b64f8a0643e2c8952e7058
VictorVillazante <villazante.bernabe.victor@gmail.com>
2023-10-17 15:44:22 -0400

    agregando prueba unitaria eliminar lugar con index 0 de lista de lugares con un lugar agregado

--- proyecto-angular-pasante5/src/app/app.component.spec.ts
+++ proyecto-angular-pasante5/src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('proyecto-angular-pasante5');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-angular-pasante5 app is running!');
-  });
 });
--- proyecto-angular-pasante5/src/app/components/formulario-guardado-mis-ubicaciones/formulario-guardado-mis-ubicaciones.component.spec.ts
+++ proyecto-angular-pasante5/src/app/components/formulario-guardado-mis-ubicaciones/formulario-guardado-mis-ubicaciones.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormularioGuardadoMisUbicacionesComponent } from './formulario-guardado-mis-ubicaciones.component';
+import { FormsModule } from '@angular/forms';
+import { MapaComponent } from '../mapa/mapa.component';
+import { AppComponent } from 'src/app/app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { APP_ROUTING } from 'src/app/app.routes';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('FormularioGuardadoMisUbicacionesComponent', () => {
   let component: FormularioGuardadoMisUbicacionesComponent;
@@ -8,7 +14,18 @@ describe('FormularioGuardadoMisUbicacionesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormularioGuardadoMisUbicacionesComponent ]
+      declarations: [
+        AppComponent,
+        MapaComponent,
+        FormularioGuardadoMisUbicacionesComponent
+      ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        APP_ROUTING,
+        FormsModule 
+    
+      ],
     })
     .compileComponents();
   });
@@ -19,7 +36,10 @@ describe('FormularioGuardadoMisUbicacionesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('eliminar lugar de lista de lugares', () => {
     expect(component).toBeTruthy();
+    component.lugares=[{nombreLugar:"lugar 1",descripcion:"descripcion",coordenadas:['-18.9120921','-17.89328392832']}];
+    component.eliminarElemento(0);
+    expect(component.lugares).toEqual([]);
   });
 });
